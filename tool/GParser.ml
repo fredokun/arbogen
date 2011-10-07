ceebd88c5e136dbd7a7c029e1a49cb3430a0c182
Frédéric Peschanski <fredericpeschanski@MacBook-Pro-de-Frederic-Peschanski-2.local>
2011-10-07 23:09:43 +0200

    parser continued' (a few bugs to track down

--- tool/GParser.ml
+++ tool/GParser.ml
@@ -17,8 +17,10 @@ type rule = string * component list
 type grammar = rule list ;;
 
 (* example of grammar *) 
-let bintree = [ ("BinNode", [ Terminal 1 ; 
-                              NonTerminal (0,["BinNode";"BinNode"]) ]) ];;
+let bintree = [ ("BinNode", [ (1,["Leaf"]) ; 
+                              (0,["BinNode";"BinNode"]) ]) ];;
+
+exception Parse_error of string;;
 
 type character =
     Char of char
@@ -34,7 +36,7 @@ let get_char str i =
 
 let rec skip_spaces str i =
   let ichar = get_char str i in
-  if is_space ichar then skip str (i+1)
+  if is_space ichar then skip_spaces str (i+1)
   else i ;;
 
 let rec skip_until_eol str i =
@@ -42,8 +44,21 @@ let rec skip_until_eol str i =
   match ichar with
     | EOF -> i
     | Char(ch) -> 
-      if ch=='\n' then i+1
+      if ch='\n' then i+1
       else skip_until_eol str (i+1);;
+
+let rec skip_until_starslash str i =
+  let ichar = get_char str i in
+  match ichar with
+    | EOF -> raise (Parse_error "Missing end of comment: */") 
+    | Char(ch) -> if ch='*' then (match get_char str (i+1) with
+        | EOF -> raise (Parse_error "Missing end of comment after *") 
+        | Char(ch') -> 
+          if ch'='/' then i+1
+          else skip_until_starslash str (i+1))
+      else skip_until_starslash str (i+1) ;;
+
+(* skip_until_starslash "/* toto titi tata */ tutu" 2 ;; *)
 
 let rec skip_comments str i =
   let ichar = get_char str i in
@@ -51,13 +66,16 @@ let rec skip_comments str i =
     | EOF -> i
     | Char(ch) -> 
       if is_space ichar then skip_comments str (i+1)
-      else if ch == '/' then
+      else if ch = '/' then
         (match get_char str (i+1) with
           | EOF -> i
           | Char(ch') -> 
-            if ch' == '/' then skip_until_eol str i 
+            if ch' = '/' then skip_until_eol str i 
+            else if ch' = '*' then skip_until_starslash str (i+1)
             else i)
       else i;;
+
+(* skip_comments "/* toto titi tata */ tutu" 0 ;;  *)
 
 let rec skip str i =
   let i' = skip_comments str i in
@@ -90,9 +108,6 @@ let next_word str i =
   in
   let (word,i') = aux (skip str i) [] in
   (string_of_list word,i') ;;
-
-
-exception Parse_error of string;;
 
 let advance str i expect = 
   let (word,i') = next_word str i
@@ -127,23 +142,58 @@ let parse_component str i =
   in
   aux i 0 [] ;;
 
+(* parse_component "<z> * BinNode * BinNode +" 0 ;; *)
+
 let parse_components str i =
   let rec aux i comps =
     let (comp,i') = parse_component str i in
     let (next,i'') = next_word str i' in
     if next="+" then
+      aux i'' (comp::comps)
+    else if next=";" then
+      (List.rev (comp::comps), i'')
+    else raise (Parse_error ("Expecting '+' or ';' after component"))
+  in
+  aux i [] ;;
+
+(* parse_components "Leaf * <z> + BinNode * BinNode ;" 0 ;;  *)
       
-    
-                            
-        
 let parse_rule str i =
   let (ruleName,i') = next_word str i in
   if ruleName="" then
-    raise Parse_error "Missing rule name"
+    raise (Parse_error "Missing rule name")
   else
     let i'' = advance str i' "::=" in
     let (components,i''') = parse_components str i''
     in
     ((ruleName,components),i''') ;;
 
+(* parse_rule "BinNode ::= Leaf * <z> + BinNode * BinNode ;" 0 ;; *)
 
+let parse_grammar str =
+  let rec aux i rules =
+    match next_word str i with
+        ("",i') -> List.rev rules
+      | _ ->
+        let (rul,i') = parse_rule str i
+        in aux i' (rul::rules)
+  in
+  aux 0 [] ;;
+
+(* parse_grammar "BinNode ::= Leaf * <z> + BinNode * BinNode ;" ;; *)
+
+
+let string_of_file fname =
+  let inchan = open_in fname in
+  let rec read str =
+    try let next = input_line inchan in
+        read (str ^ "\n" ^ next)
+    with End_of_file -> close_in inchan ; str
+  in
+  read "" ;;
+
+let parse_from_file fname =
+  let input = string_of_file fname in
+  parse_grammar input ;;
+
+parse_from_file "examples/binary.arb" ;;
