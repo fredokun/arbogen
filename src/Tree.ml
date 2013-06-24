70af8b451e95938d03cf1b79a6b9baa112f55279
Marwan Ghanem <marwan.t.ghanem@gmail.com>
2013-06-24 15:12:41 +0200

    leaf fix

--- src/Tree.ml
+++ src/Tree.ml
@@ -45,7 +45,7 @@ let rec tree_out show_type show_id tree out =
        else "")
   in
     match tree with
-    | Leaf(typ,id) -> output_string out ((label typ id) ^ ".")
+    | Leaf(typ,id) -> output_string out (label typ id)
     | Node(typ,id,ts) ->
       output_string out (label typ id) ;
       output_list 
