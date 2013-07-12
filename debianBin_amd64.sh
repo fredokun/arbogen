f55676c3e9cef0f67b1024cc0a0bf094bba0f95a
Marwan <marwan.t.ghanem@gmail.com>
2013-07-12 19:52:52 +0200

    packaging scripts fix

--- debianBin_amd64.sh
+++ debianBin_amd64.sh
@@ -4,6 +4,14 @@
 
 file=arbogen_`cat VERSION`_1
 
+#testing if in correct folder
+if [ ! -f configure.in ]; then
+    echo "checking if in correct folder... no"
+    exit 1;
+else
+    echo "checking if in correct folder... ok"
+fi
+
 if which dpkg >/dev/null; then 
 	echo "checking for dpkg... ok"
 else
@@ -31,7 +39,7 @@ mkdir -p $file/usr/share/man/man1
 #creation of text files
 cat <<EOF > $file/DEBIAN/control
 Package: arbogen
-Version: $1
+Version: `cat VERSION`
 Section: ocaml
 Priority: optional
 Source: arbogen
@@ -44,10 +52,17 @@ EOF
 cp doc/arbogen.1 $file/usr/share/man/man1/arbogen.1
 gzip --best $file/usr/share/man/man1/arbogen.1
 cp LICENSE.txt $file/usr/share/doc/arbogen/copyright
-cp arbogen $file/usr/bin/
+
+#compiling to create a executable.
+./compile.sh
+cp bin/arbogen $file/usr/bin/
+
 
 chmod -R 755 $file
 chmod a-x   $file/usr/share/man/man1/arbogen.1.gz
 chmod a-x $file/usr/share/doc/arbogen/copyright
 #packaging
 fakeroot dpkg-deb --build $file
+
+#cleaning up
+./cleanup.sh
