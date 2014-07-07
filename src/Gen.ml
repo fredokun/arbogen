16c5857dba590d099985705eb8a7e389be2fe2ac
Marwan Ghanem <marwan.t.ghanem@gmail.com>
2014-07-07 02:39:29 +0200

    end of day, maybe problem with weight

--- src/Gen.ml
+++ src/Gen.ml
@@ -23,157 +23,65 @@ open Grammar
 
 
 
-(* let pondere2 (g:grammar) (y:float array)
-	(*: (float StringMap * (string list * float) list StringMap )*) =
-   let g_comp = completion g in
-   let ymap =
-   List.fold_left2
-   (fun map coef (name,_) -> StringMap.add name coef map)
-   (StringMap.empty)
-   (Array.to_list y)
-   g_comp
-   in
-  (* calcule la pondération correspondante à un sous composants *)
-   let aux2 = fun (_,componentList) ->
-   let proba =
-   List.fold_left
-	 (fun a b -> a *. (StringMap.find (name_of_elem b) ymap))
-	 1.
-	 componentList
-   in
-   let len = if (List.length componentList = 1) &&
-	 (List.exists (fun x -> x = (name_of_elem (List.hd componentList))) (leafs_of_grammar g)) then 0
-   else List.length componentList
-   in
-   (componentList,len,proba)
-   in
-  (* renvoie la map des composants avec leurs sous composants (prochain fils) et pondération *)
-   let aux = fun map (component,subComponents) ->
-   let sub_component_list = List.map aux2 subComponents
-   in
-   let proba = (List.fold_left (fun a (_,_,f) -> f +. a) 0. sub_component_list)
-   in
-   StringMap.add
-   component
-   (sub_component_list,proba)
-   map
-   in
-   List.fold_left aux StringMap.empty g_comp *)
-
 let rec find_component rdm_float componentList = 
   match componentList with
-  | [comp] -> comp
-  | comp::list_comp -> let (composant,freq) = comp in
-                       if rdm_float <= freq then
-                         comp
-                       else
-                         find_component (rdm_float-.freq) list_comp
-  | _ -> failwith "find_component failed !!!" 
+    | [comp] -> comp
+    | comp::list_comp -> let (composant,freq) = comp in
+			 if rdm_float <= freq then
+                           comp
+			 else
+                           find_component (rdm_float-.freq) list_comp
+    | _ -> failwith "find_component failed !!!" 
 
 let rec get_next_rule (name_rule:string) (wgrm:WeightedGrammar.weighted_grammar) =      
   let (total_weight,component_list) = (StringMap.find name_rule wgrm) in
-  let rdm_float = Random.float total_weight in 
+  let rdm_float = Random.float total_weight in
   let comp = (find_component rdm_float component_list) in
   match comp with
-  | (Grammar.Call elem), _ -> get_next_rule elem wgrm 
-  | (Grammar.Cons( _, elem_list)), _ ->
-    List.fold_left 
-      (fun next_rules elem ->
-        match elem with
-        | (Grammar.Elem name) -> name :: next_rules
-        | (Grammar.Seq name) -> let (w,_) = StringMap.find name_rule wgrm in
-                                let n' = int_of_float (floor((log( Random.float 1.)) /. (log w))) in
-                                         next_rules @ (concat_n [name] (n'-1))
-      )
-      []
-      elem_list
+    | (Grammar.Call elem), _ -> get_next_rule elem wgrm 
+    | (Grammar.Cons( w, elem_list)), _ ->
+      List.fold_left 
+	(fun next_rules elem ->
+          match elem with
+            | (Grammar.Elem name) -> name :: next_rules
+            | (Grammar.Seq name) -> let (w,_) = StringMap.find name_rule wgrm in
+                                    let n' = int_of_float (floor((log( Random.float 1.)) /. (log w))) in
+                                    next_rules @ (concat_n [name] (n'-1))
+	)
+	[]
+	elem_list
+
 
 let rec count_rules counters elements =
   match elements with 
-  |elem::elems -> let nb = StringMap.find elem counters in
-                  let new_map = StringMap.add elem (nb+1) counters in
-                  count_rules new_map elems;
-  | _ -> counters
+    |elem::elems -> let nb = StringMap.find elem counters in
+                    let new_map = StringMap.add elem (nb+1) counters in
+                    count_rules new_map elems;
+    | _ -> counters
 
 let  find_non_zero counters = 
   let filterd_map = StringMap.filter (fun _ n -> -n <> 0) counters in
   fst (StringMap.choose filterd_map)
 
-let rec sim(size:float) counters (wgrm:WeightedGrammar.weighted_grammar) (sizemax:float) leafs (current_rule:string) =
-  if (StringMap.for_all (fun _ n -> n == 0 ) counters) || (size>sizemax)  then
+let rec sim(size:float) counters (wgrm:WeightedGrammar.weighted_grammar) (sizemax:float) (current_rule:string) =
+  if (StringMap.for_all (fun _ n -> n = 0 ) counters) || (size>sizemax)  then
     size
   else
-    if (List.exists (fun n -> (n == current_rule)) leafs) then
-      let(total_weight,_) = (StringMap.find current_rule wgrm) in
-      sim (size+.total_weight) counters wgrm sizemax leafs (find_non_zero counters)
+    let(total_weight,_) = (StringMap.find current_rule wgrm) in
+    printf "total_weight = %f\n" total_weight; 
+    let next_rules = get_next_rule current_rule wgrm in
+    if (List.length next_rules) > 0 then
+      let new_counters = (count_rules counters (List.tl next_rules)) in  
+      sim (size+.total_weight) new_counters wgrm sizemax  (List.hd next_rules)
     else
-      let next_rules = get_next_rule current_rule wgrm in
-      let new_counters = (count_rules counters (List.tl next_rules)) in 
-          printf "%s\n" (List.hd next_rules);
-          sim (size) new_counters wgrm sizemax leafs (List.hd next_rules)
-
+      let non_zero = find_non_zero counters in
+      let nb = StringMap.find non_zero counters in
+      let new_nb = nb - 1 in
+      sim (size+.total_weight) (StringMap.add non_zero new_nb counters) wgrm sizemax non_zero
+        
 let rec init_counter g map =
   match g with
-  | rul::rules -> StringMap.add (fst rul) 0 map
-  | _ -> map 
+    | rul::rules -> StringMap.add (fst rul) 0 map
+    | _ -> map 
  
-(* 
-let generator
-    (g:grammar)
-    (self_seed:bool) (seed:int)
-    (sizemin:int) (sizemax:int)
-    (epsilon1:float) (epsilon1_factor:float)
-    (epsilon2:float) (epsilon2_factor:float)
-    (with_prefix:bool) (idprefix:string)
-    (max_try:int) (ratio_rejected:float)
-    (max_refine:int)(zstart:float)
-    : (tree*int) option = 
-  let sys = combsys_of_grammar(completion g) in
-  let rec gen epsilon1 epsilon2 zmin zmax nb_refine zstart =
-    let (zmin',zmax',y) = 
-      (if global_options.verbosity >= 2
-       then printf "[ORACLE]: search singularity at z=%f\n%!" zstart) ;
-      searchSingularity sys zmin zmax epsilon1 epsilon2 zstart in
-    (if global_options.verbosity >= 2
-     then printf "          ==> found singularity at z=%f\n%!" zmin') ; 
 
-          (* let rec try_gen(nb_try:int)(nb_smaller:int) : ((tree * int) option * int * int) =
-          *)  
-    Random.init 123123;
-    let leafs = leafs_of_grammar g in 
-    let(first_rule,_) = List.hd g in 
-    let counters = StringMap.empty in
-          (* need to fill the counter with 0 ?? *)
-    let res = sim 0 counters wgrm sizemax (fst first_rule) in
-    printf "res  : %d\n" res;
-  in
-  gen epsilon1 epsilon2 0. 1. 1 zstart  *)
-
-(* if self_seed
-   then
-   Random.init seed;
-   else
-   let rnd = Random.int 3142342 in
-   Random.init rnd;
-
-   let map = pondere2 g y in
-   let leafs = leafs_of_grammar g in
-   let (first_rule,_) = List.hd g in
-   let counters = StringMap.empty in
-   StringMap.add first_rule 1 counters; 
-   let res = sim 0 counters map leafs sizemax in
-   if res >= sizemin && res <= sizemax then
-   begin
-   Ranom.init seed;
-   gen_tree g with_prefix idprefix sizemax y;
-   end
-   else
-   if self_seed then
-   failwith "Your trees are too big, change paramaters please"
-   else if nb_try > 0 then
-   if res < sizemin then
-   try_gen (nb_try-1) (nb_smaller+1) nb_bigger  
-   else
-   try_gen (nb_try-1) nb_smaller (nb_bigger+1)       (* Partie de rafinement on a le besoin ou pas ??? *)
-   in
-   gen epsilon1 epsilon2 0. 1. 1 zstart  *)
--- src/Test.ml
+++ src/Test.ml
@@ -18,26 +18,24 @@ let _ =
       let grammar = Ast.grammar_of_ast ast in
       let combsys = combsys_of_grammar grammar in
       let (zmin,zmax,y) = searchSingularity combsys 0. 0.9 0.01 0.01 0.8 in
-      let wgrm = weighted_grm_of_grm grammar y in
-      let leafs = leafs_of_grammar grammar in 
-      let counters = init_counter grammar StringMap.empty in
-      let(first_rule,_) = List.hd grammar in 
-      let res = sim 10.0 counters wgrm 100.0 leafs (fst first_rule) in
-        printf "res %d\n" res; 
-        
+      let wgrm = weighted_grm_of_grm grammar y in        
       print_endline (string_of_grammar grammar);
       print_newline();
       print_endline (string_of_weighted_grammar wgrm);
+      (* print_endline (fst @@ string_of_combsys combsys); wont compile on my pc therefore was commented*)
+      print_newline();
 
-      (* print_endline (fst @@ string_of_combsys combsys); *)
-      (* print_newline();
+      Random.self_init () ;
+      let counters = init_counter grammar StringMap.empty in
+      let(first_rule,_) = List.hd grammar in 
+      let new_counters = StringMap.add first_rule 1 counters in
+      let res = sim 0.0 new_counters wgrm 100.0 first_rule in
+      printf "Size of Tree simulated %f\n" res; 
+
+      
       
       print_newline();
-      flush stdout;    *)
-      (* let counters = init_counter grammar StringMap.empty in
-      let(first_rule,_) = List.hd grammar in 
-      let res = sim 10.0 counters wgrm 100.0 leafs (fst first_rule) in
-        printf "res %d\n" res; *)
+      flush stdout;   
     done
   with End_of_file ->
     failwith "Zut"
