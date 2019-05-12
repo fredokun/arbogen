28b649d81c60c89c97c1a3fdb3e6cf84650da514
Martin Pépin <kerl@wkerl.me>
2019-05-12 21:52:30 +0200

    optimisation: reuse previous computations in OracleSimple

--- src/oracleSimple.ml
+++ src/oracleSimple.ml
@@ -36,7 +36,7 @@ let diverge (epsilon2: float) : float array -> bool =
   let is_nan x = x <> x in
   Array.exists (fun x -> x > too_big || x < 0. || is_nan x)
 
-let iteration_simple grammar z epsilon2 =
+let iteration_simple grammar z ?init_values epsilon2 =
   let rec iterate v1 v2 =
     let _ = Oracle.(eval ~dest:v2 {z; values = v1}) grammar in
     if diverge epsilon2 v2 then
@@ -48,17 +48,20 @@ let iteration_simple grammar z epsilon2 =
   in
   (* Only allocate to arrays and swap them at each iteration *)
   let len = Array.length grammar.Grammar.rules in
-  let v1 = Array.make len 0. in
+  let v1 = match init_values with
+    | None -> Array.make len 0.
+    | Some values -> Array.copy values
+  in
   let v2 = Array.make len 0. in
   iterate v1 v2
 
 let search_singularity {epsilon1; epsilon2; zmin; zmax; zstart} grammar =
-  let rec search zmin zmax zstart =
+  let rec search ?init_values zmin zmax zstart =
     if zmax -. zmin < epsilon1 then
-      (zmin, zmax, iteration_simple grammar zmin epsilon2)
+      (zmin, zmax, iteration_simple grammar zmin ?init_values epsilon2)
     else
       match iteration_simple grammar zstart epsilon2 with
-      | Val _ -> search zstart zmax ((zmax +. zstart) /. 2.)
+      | Val values -> search ~init_values:values zstart zmax ((zmax +. zstart) /. 2.)
       | Diverge -> search zmin zstart ((zmin +. zstart) /. 2.)
   in
   match search zmin zmax zstart with
