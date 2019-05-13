95cf2831ba60f183987549b6e5bbdb8a5c85f62e
Martin Pépin <kerl@wkerl.me>
2019-05-13 17:37:12 +0200

    remove unused code from Util

--- src/util.ml
+++ src/util.ml
@@ -14,9 +14,6 @@
 
 (* List utilities *)
 
-let fold_map mop fop finit a =
-  List.fold_left (fun r e -> fop (mop e) r) finit a
-
 let string_of_list_buf str_of_elem buf op dl cl l =
   let rec aux = function
     | [] -> Buffer.add_string buf cl
@@ -43,12 +40,3 @@ let rec output_list out output_elem op dl cl l = match l with
     output_elem out e ;
     output_string out dl ;
     output_list out output_elem "" dl cl l'
-
-(* Array utilities *)
-
-let array_for_all2 p u v =
-  if Array.length u <> Array.length v then invalid_arg "array_for_all2";
-  try
-    Array.iter2 (fun x y -> if not (p x y) then raise Exit) u v;
-    true
-  with Exit -> false
